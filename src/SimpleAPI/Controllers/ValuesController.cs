d90e012a813aebf19451d52b12c5625ddc8f9095
uttam.chaturvedi <uttam.chaturvedi@ccsgtestcustomer03.onmicrosoft.com>
2020-03-21 10:41:29 +0100

    Code Added

--- src/SimpleAPI/Controllers/ValuesController.cs
+++ src/SimpleAPI/Controllers/ValuesController.cs
@@ -21,7 +21,7 @@ namespace SimpleAPI.Controllers
         [HttpGet("{id}")]
         public ActionResult<string> Get(int id)
         {
-            return "Uttam Chaturvedi";
+            return "Uttam Chaturvedi Test";
         }
 
         // POST api/values
